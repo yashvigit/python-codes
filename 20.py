5635872a67c250412972cc37d5cb767646a3e9ad
yashvigit <24BIT013@sot.pdpu.ac.in>
2025-01-20 22:26:22 +0530

    Delete 20.py

--- 20.py
+++
@@ -1,5 +0,0 @@
-#Calculate area of a triangle. A = H*L/2
-l=float(input("enter the lenght of triangle:"))
-h=float(input("enter the height of triangle:"))
-a=h*l/2
-print("the area of triangle is:",a)
